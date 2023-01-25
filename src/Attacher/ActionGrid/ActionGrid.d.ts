f460fa8e1da159385f627adb89853ece806a7c19
AhmedAyachi <aayachi032@gmail.com>
2023-01-25 17:31:35 +0100

    Attacher/ActionGrid: fix docs

--- src/Attacher/ActionGrid/ActionGrid.d.ts
+++ src/Attacher/ActionGrid/ActionGrid.d.ts
@@ -3,7 +3,7 @@ import {ViewInterface as View} from "vanilla"
 
 interface ActionGrid extends View {
     /**
-     * Fades out the ActionGrid and removes ir from DOM
+     * Fades out the ActionGrid and removes it from DOM
      */
     unmount():void,
 }
